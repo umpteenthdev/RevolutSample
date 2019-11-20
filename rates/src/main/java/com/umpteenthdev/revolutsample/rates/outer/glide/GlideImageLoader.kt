af40991c3baec4f0e7982be6bddb40cc7f8fcda7
umpteenthdev <andrey.solovetov@gmail.com>
2019-11-20 10:37:29 +0300

    Log Glide errors as nonfatal

--- rates/src/main/java/com/umpteenthdev/revolutsample/rates/outer/glide/GlideImageLoader.kt
+++ rates/src/main/java/com/umpteenthdev/revolutsample/rates/outer/glide/GlideImageLoader.kt
@@ -1,7 +1,13 @@
 package com.umpteenthdev.revolutsample.rates.outer.glide
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.umpteenthdev.revolutsample.core.entity.sendNonFatalException
 import com.umpteenthdev.revolutsample.rates.adapters.imageloading.ImageLoader
 import javax.inject.Inject
 
@@ -11,6 +17,17 @@ class GlideImageLoader @Inject constructor() : ImageLoader {
         Glide.with(view.context)
             .load(imageUrl)
             .placeholder(placeholderRes)
+            .addListener(object : RequestListener<Drawable?> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable?>?, isFirstResource: Boolean): Boolean {
+                    if (e == null) return false
+                    sendNonFatalException(e)
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable?>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    return false
+                }
+            })
             .into(view)
     }
 }
